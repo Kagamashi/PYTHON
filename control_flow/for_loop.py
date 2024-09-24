b9e2b9b72f7472b9d175154bb7c76635de9e6fc6
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-09-24 09:26:57 +0200

    add control flow

---
+++ control_flow/for_loop.py
@@ -0,0 +1,25 @@
+'''
+for loop is used to iterate over a sequence (list, tuple, string) or any other literable object
+it allows to execute a block of code for each item in the sequence
+'''
+
+# Iterate over a list
+numbers = [1, 2, 3, 4]
+for number in numbers:
+    print(number)
+
+# Iterate over a string
+for char in "Python":
+    print(char)
+
+# Output:
+# 1
+# 2
+# 3
+# 4
+# P
+# y
+# t
+# h
+# o
+# n
