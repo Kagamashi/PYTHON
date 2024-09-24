a2aefd552a5f5b5d0df2fb4dd03d0043df245d1b
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-09-24 09:02:57 +0200

    explain how comments work in python

---
+++ syntax/comments.py
@@ -0,0 +1,15 @@
+# Single line comments start with : #
+# Multi-line comments don't exist in python natively, but we can use triple quotes
+# to create multi-line string that acts as a comment ''' or """
+
+
+# This is a single-line comment
+
+'''
+This is a multi-line comment
+that spans multiple lines.
+It can be used for documentation or explanations.
+'''
+
+# Example code with comments
+x = 10  # This is a variable storing the number 10
