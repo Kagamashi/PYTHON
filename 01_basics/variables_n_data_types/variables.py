f07bdfb80e7449fbbe42089c442b9c400429bee9
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-08 17:31:13 +0100

    Modified: 01_basics/variables_n_data_types/variables.py

---
+++ 01_basics/variables_n_data_types/variables.py
@@ -0,0 +1,16 @@
+'''
+Variables are assigned values using = operator
+
+Constants don't exist natively in Python.
+By convention constants are written in all UPPERCASE LETTERS
+[!] They should not be changed during the execution of the program
+'''
+
+# Variable Assignment
+x = 10  # Integer
+y = 3.14  # Float
+name = "Alice"  # String
+
+# Constants (by convention, uppercase)
+PI = 3.14159
+GRAVITY = 9.8
