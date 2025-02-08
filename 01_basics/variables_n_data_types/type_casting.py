24891bccd094e9b6df169b1217bf4ecd5853a749
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-08 17:31:13 +0100

    Added new file: 01_basics/variables_n_data_types/type_casting.py

---
+++ 01_basics/variables_n_data_types/type_casting.py
@@ -0,0 +1,36 @@
+# TYPE CASTING 
+'''
+Typecasting (type conversion) is the proces of converting one data type to another
+Python supports several built-in functions for typecasting
+
+int(): Converts a value to an integer.
+float(): Converts a value to a float.
+str(): Converts a value to a string.
+bool(): Converts a value to a boolean.
+'''
+
+# Converting float to int
+x = 3.99
+y = int(x)  # y will be 3 (floats are truncated when cast to int)
+
+# Converting int to float
+z = float(10)  # z will be 10.0
+
+# Converting int to string
+a = str(123)  # a will be '123'
+
+# Converting string to int
+b = int("45")  # b will be 45
+
+# Boolean conversion
+is_valid = bool(1)  # True (non-zero values are True)
+is_empty = bool(0)  # False (0 is False)
+
+
+
+
+# DUCK TYPING
+
+
+
+
