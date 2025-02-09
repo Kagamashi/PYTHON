c122a7da68d23d0f3782fe761288ac9c35e6453d
Mikolaj <mikolaj.borecki1@gmail.com>
2025-02-09 20:29:12 +0100

    Modified: 01_basics/data_strucures/tuples.py

--- 01_basics/data_strucures/tuples.py
+++ 01_basics/data_strucures/tuples.py
@@ -1,10 +1,20 @@
 '''
+(A, B, C)
 Tuple is ordered, immutable collection of items.
 Once created, cannot be changed or modified.
+- allows duplicates
+- can contain elements of different data types.
+
+    len()
+
 '''
 
-my_tuple = (1, 2, 3)
-print(my_tuple)  # Output: (1, 2, 3)
+my_tuple = (1, 2, 3) # Output: (1, 2, 3)
 
 # Accessing elements
 print(my_tuple[1])  # Output: 2
+
+# One item tuple
+thistuple = ("apple",) # comma needed
+
+
