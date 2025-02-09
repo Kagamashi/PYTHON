06ac35dcc5def39da5909abf8b036d10070116ac
Mikolaj <mikolaj.borecki1@gmail.com>
2025-02-09 21:46:18 +0100

    Modified: 01_basics/data_strucures/sets.py

--- 01_basics/data_strucures/sets.py
+++ 01_basics/data_strucures/sets.py
@@ -1,20 +1,23 @@
 '''
+{A, B, C}
 Set is unordered, collection of unique items.
-Sets do not allow duplicates and are useful for operations like union, intersection and difference.
-Methods:
-  add(item): Adds an item to the set.
-  remove(item): Removes an item (raises an error if it doesn’t exist).
-  union(): Returns a new set with all items from both sets.
-  intersection(): Returns a set with only the items present in both sets.
-  difference(): Returns a set with items present in the first set but not in the second.
+- do not allow duplicates
+- useful for operations like union, intersection and difference
+- can contain different data types
+
+  add(item)
+  remove(item)
+  union():        Returns a new set with all items from both sets.
+  intersection(): Returns a set with only the items present in both sets [&]
+  difference():   Returns a set with items present in the first set but not in the second
+  symmetric_difference():   Keeps all items EXCEPT the duplicates
 '''
 
 my_set = {1, 2, 3}
-my_set.add(4)  # Adds 4 to the set
-print(my_set)  # Output: {1, 2, 3, 4}
+my_set.add(4)  # {1, 2, 3, 4}
 
 # Set operations
 set_a = {1, 2, 3}
 set_b = {3, 4, 5}
-print(set_a.union(set_b))  # Output: {1, 2, 3, 4, 5}
+print(set_a.union(set_b))         # Output: {1, 2, 3, 4, 5}
 print(set_a.intersection(set_b))  # Output: {3}
