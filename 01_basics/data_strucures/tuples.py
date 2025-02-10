dd13837270f122f5338a4a57ce73e0b8017b66c2
Mikolaj <mikolaj.borecki1@gmail.com>
2025-02-10 20:41:59 +0100

    Modified: 01_basics/data_strucures/tuples.py

--- 01_basics/data_strucures/tuples.py
+++ 01_basics/data_strucures/tuples.py
@@ -4,6 +4,7 @@ Tuple is ordered, immutable collection of items.
 Once created, cannot be changed or modified.
 - allows duplicates
 - can contain elements of different data types.
+- negative indexing means start from the end
 
     len()
     count()
