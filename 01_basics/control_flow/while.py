30d837f63ed2d1924e9b98c5394f1e8b0ede32b7
Mikolaj <mikolaj.borecki1@gmail.com>
2025-02-10 20:41:58 +0100

    Modified: 01_basics/control_flow/while.py

--- 01_basics/control_flow/while.py
+++ 01_basics/control_flow/while.py
@@ -1,15 +1,18 @@
 '''
 whie loop repeats as long as the specified condition is True
+
+- break statement stops the loop even if the condition is true
+- continue statement stops current iteration
+- else statement runs when the condition is no longer true
 '''
 
-x = 0
-while x < 5:
-    print(x)
-    x += 1  # Increment x by 1
-
-# Output:
-# 0
-# 1
-# 2
-# 3
-# 4
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  if i == 5:
+    continue
+  i += 1
+else:
+  print("i is no longer less than 6")
