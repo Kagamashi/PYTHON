a71f7e008c5d0cf499001f9fc1a3beb5eaa56e5e
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-13 21:06:22 +0100

    Deleted: packages/automation/Selenium.py

--- packages/automation/Selenium.py
+++
@@ -1,7 +0,0 @@
-# Selenium
-# Selenium automates browsers, enabling tasks such as web scraping or testing.
-from selenium import webdriver  # type: ignore
-driver = webdriver.Chrome()
-driver.get('https://www.example.com')
-print(driver.title)
-driver.quit()
