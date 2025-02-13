53965c21ea95576eced4b10d22f2fd6d3280d3f3
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-13 21:06:22 +0100

    Deleted: packages/GUI/Tkinter.py

--- packages/GUI/Tkinter.py
+++
@@ -1,7 +0,0 @@
-# Tkinter
-# Tkinter is a built-in Python library for creating graphical user interfaces (GUIs).
-import tkinter as tk  # type: ignore
-root = tk.Tk()
-label = tk.Label(root, text="Hello, Tkinter!")
-label.pack()
-root.mainloop()
