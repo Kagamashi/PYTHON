533bbfc17fda64a3da8b600ddb751a0eca33d70f
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-13 21:06:23 +0100

    Added new file: 03_packages/

---
+++ 03_packages/packages/data/pandas.py
@@ -0,0 +1,5 @@
+# pandas
+# pandas is used for data manipulation and analysis, especially for working with tabular data (DataFrames).
+import pandas as pd  # type: ignore
+df = pd.read_csv('data.csv')
+print(df.head())
