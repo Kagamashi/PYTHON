4e0f39ade797ee93146eb1e9c5d390a0a7e7e29f
Mikolaj <mikolaj.borecki1@gmail.com>
2025-02-13 22:11:26 +0100

    Added new file: 03_packages_and_dependencies/

---
+++ 03_packages_and_dependencies/packages/data/matplotlib.py
@@ -0,0 +1,7 @@
+# matplotlib
+# matplotlib is used for creating static, interactive, and animated visualizations in Python.
+import matplotlib.pyplot as plt  # type: ignore
+x = [1, 2, 3, 4]
+y = [10, 20, 25, 30]
+plt.plot(x, y)
+plt.show()
