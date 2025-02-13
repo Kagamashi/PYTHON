31f99cbb59c788b93d97272c23c7c22b66d1e053
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-13 21:06:22 +0100

    Deleted: packages/data/numpy.py

--- packages/data/numpy.py
+++
@@ -1,5 +0,0 @@
-# numpy
-# numpy provides support for large multi-dimensional arrays and matrices, along with a collection of mathematical functions to operate on these arrays.
-import numpy as np  # type: ignore
-arr = np.array([1, 2, 3, 4])
-print(arr + 2)
