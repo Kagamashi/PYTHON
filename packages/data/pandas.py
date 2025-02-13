b09516d5177dbc121097b79e76a87f8863659e1d
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-13 21:06:22 +0100

    Deleted: packages/data/pandas.py

--- packages/data/pandas.py
+++
@@ -1,5 +0,0 @@
-# pandas
-# pandas is used for data manipulation and analysis, especially for working with tabular data (DataFrames).
-import pandas as pd  # type: ignore
-df = pd.read_csv('data.csv')
-print(df.head())
