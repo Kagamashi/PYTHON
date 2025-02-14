d75df274ef7dc955c501fab7e8f97b1c4aad521d
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-14 09:27:55 +0100

    Deleted: 03_packages_and_dependencies/packages/data/matplotlib.py

--- 03_packages_and_dependencies/packages/data/matplotlib.py
+++
@@ -1,7 +0,0 @@
-# matplotlib
-# matplotlib is used for creating static, interactive, and animated visualizations in Python.
-import matplotlib.pyplot as plt  # type: ignore
-x = [1, 2, 3, 4]
-y = [10, 20, 25, 30]
-plt.plot(x, y)
-plt.show()
