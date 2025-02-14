5016aab89da14eaffaed16fe18cc5d77358b217c
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-14 10:46:30 +0100

    Modified: 05_toolkits_and_ecosystem/cloud_integration/terraform.py

--- 05_toolkits_and_ecosystem/cloud_integration/terraform.py
+++ 05_toolkits_and_ecosystem/cloud_integration/terraform.py
@@ -4,7 +4,7 @@ python-terraform is used to interact with Terraform CLI inside python scripts
 'pip install python-terraform'
 '''
 
-from python_terraform import Terraform
+from python_terraform import Terraform # type: ignore
 
 tf = Terraform(working_dir="./terraform")
 tf.init()
