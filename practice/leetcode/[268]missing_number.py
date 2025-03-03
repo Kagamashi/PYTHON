175cb9fbf8e58b5299f2c0d047902137680802cb
Mikolaj <mikolaj.borecki1@gmail.com>
2025-03-03 22:42:54 +0100

    Modified: practice/leetcode/[268]missing_number.py

--- practice/leetcode/[268]missing_number.py
+++ practice/leetcode/[268]missing_number.py
@@ -13,12 +13,12 @@ range object creation = O(1)
 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
 
-        for i,v in enumerate(nums):
-            if (i != v):
-                return v-1
-            if v == len(nums)-1:
-                return v+1
+        # nums.sort()
+        # for i,v in enumerate(nums):
+        #     if (i != v):
+        #         return v-1
+        #     if v == len(nums)-1:
+        #         return v+1
 
         return sum(range(len(nums)+1))-sum(nums)
